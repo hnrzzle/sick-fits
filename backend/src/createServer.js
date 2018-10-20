ad7f6fd5bb8b9b6293466835df8abc7ad3c71122
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-10-19 18:11:00 -0700

    get server up and running

---
+++ backend/src/createServer.js
@@ -0,0 +1,20 @@
+const db = require('./db');
+const { GraphQLServer } = require('graphql-yoga');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+
+function createServer() {
+  return new GraphQLServer({
+    typeDefs: 'src/schema.graphql',
+    resolvers: {
+      Mutation,
+      Query
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({ ...req, db })
+  })
+}
+
+module.exports = createServer;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,18 @@
-// let's go!
+require('dotenv').config();
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO Express middleware to handle cookies (JWT)
+// TODO Express middleware to populate current user
+
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+}, details => {
+  console.log(`Server is now running on port http://localhost:${details.port}`);
+});
