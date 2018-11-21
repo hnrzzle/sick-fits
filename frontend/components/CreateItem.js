37c54d0e26d164be1e63084bed71dc21a45d7312
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-11-20 20:51:43 -0800

    handle errors and loading for sell form

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -3,6 +3,27 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
+
+const CREATE_ITEM_MUTATION = gql`
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+    title: $title
+    description: $description
+    price: $price
+    largeImage: $largeImage
+    image: $image
+    ) {
+      id
+    }
+  }
+`;
 
 class CreateItem extends Component {
   state = {
@@ -24,47 +45,59 @@ class CreateItem extends Component {
     const { title, description, image, largeImage, price } = this.state;
 
     return (
-      <Form>
-        <fieldset>
-          <label htmlFor="title">
-            Title
-            <input 
-            type="text" 
-            id="title" 
-            name="title" 
-            placeholder="Title" 
-            required 
-            value={title}
-            onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="price">
-            Title
-            <input 
-            type="number" 
-            id="price" 
-            name="price" 
-            placeholder="Price" 
-            required 
-            value={price}
-            onChange={this.handleChange}
-            />
-          </label>
-          <label htmlFor="description">
-            Description
-            <textarea 
-            id="description" 
-            name="description" 
-            placeholder="Description" 
-            required 
-            value={description}
-            onChange={this.handleChange}
-            />
-          </label>
-        </fieldset>
-      </Form>
+      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state} >
+        {(createItem, { loading, error }) => (
+        <Form onSubmit={(e) => {
+          e.preventDefault();
+          console.log(this.state);
+          }}
+        >
+          <Error error={error} />
+          <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="title">
+              Title
+              <input 
+              type="text" 
+              id="title" 
+              name="title" 
+              placeholder="Title" 
+              required 
+              value={title}
+              onChange={this.handleChange}
+              />
+            </label>
+            <label htmlFor="price">
+              Title
+              <input 
+              type="number" 
+              id="price" 
+              name="price" 
+              placeholder="Price" 
+              required 
+              value={price}
+              onChange={this.handleChange}
+              />
+            </label>
+            <label htmlFor="description">
+              Description
+              <textarea 
+              id="description" 
+              name="description" 
+              placeholder="Description" 
+              required 
+              value={description}
+              onChange={this.handleChange}
+              />
+            </label>
+            <button type="submit">Submit</button>
+          </fieldset>
+        </Form>
+      )}
+    </Mutation>
     );
   }
 }
 
 export default CreateItem;
+
+export { CREATE_ITEM_MUTATION };
